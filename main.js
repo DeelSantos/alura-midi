e4fd1987187e33ced04910ca9256c64f9e942ed4
Deel Santos <edelsonjr.4@hotmail.com>
2022-09-15 09:54:07 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function listaDeTeclas () {
+    document.querySelectorAll('.teclas').play();
 }
 
-function tocaSomClap () {
-    document.querySelector('#som_tecla_clap').play();
-}
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = listaDeTeclas
+
 
