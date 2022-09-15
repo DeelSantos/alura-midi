7e6a46c85ee8e791bf410417ff40a580df28cad6
Deel Santos <edelsonjr.4@hotmail.com>
2022-09-15 14:14:23 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-function listaDeTeclas () {
+function tocaSomPom () {
     document.querySelectorAll('.teclas').play();
 }
 
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = listaDeTeclas
+listaDeTeclas[0].onclick = tocaSomPom;
+
+while (contador < 9) {
+    listaDeTeclas[0].onclick = tocaSomPom;
+
+    contador += 1; 
+}
 
 
