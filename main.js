ecf6a4e3563d2d8dc063341cc9bd818572dd7261
Deel Santos <edelsonjr.4@hotmail.com>
2022-10-10 10:36:22 -0300

    Update main.js

--- main.js
+++ main.js
@@ -10,18 +10,24 @@ for (let cont = 0; cont < listaDeTeclas.length; cont++) {
 
     const tecla = listaDeTeclas[cont];
     const instrumento = tecla.classList[1];
-    //template string
-
     const idAudio = `#som_${instrumento}`;
-    //console.log(idAudio);
+ 
 
 
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
  
+    tecla.onkeydown = function (evento) {
 
+   
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
     
 }
-
 
